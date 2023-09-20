7a813172abde889661da52c85b575a85de6fa79b
Patarot <adele.patarot@gmail.com>
2023-09-20 11:55:08 +0200

    exercice Date, non achevé

---
+++ TP-02-pointeurs/03-Dates/main.c
@@ -0,0 +1,74 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+//Declarations
+enum Mois {
+   JANVIER, FEVRIER, MARS, AVRIL, MAI, JUIN, JUILLET, AOUT, SEPTEMBRE, OCTOBRE, NOVEMBRE, DECEMBRE
+};
+
+struct Date {
+   int jour;
+   enum Mois mois;
+   int annee; 
+};
+typedef struct Date Date;
+
+//Prototype des fonctions 
+void initialiseDate(Date *pDate);
+void afficheDate(Date *pDate);
+Date creerDateParCopie ();
+Date* newDate();
+
+int main() {
+   //declaration de ma date et de son pointeur
+   Date date;
+   Date *pDate = &date;
+   printf("Question b\n");
+   initialiseDate(pDate);
+   afficheDate(pDate);
+
+   printf("Question c\n");
+   Date nouvelleDate;
+   nouvelleDate = creerDateParCopie();
+   afficheDate(&nouvelleDate);
+
+   printf ("Question d\n");
+   Date* myNewDate; // new date est un pointeur
+   myNewDate = newDate(); 
+   afficheDate(myNewDate);
+
+
+
+}
+
+//Question B] Initialise la date en parametre
+void initialiseDate(Date *pDate) {
+   printf("Quelle est votre date de naissance?\n");
+   printf("format: DD MM YYYY\n");
+   scanf("%d %d %d", &pDate->jour, (int*)&pDate->mois, &pDate->annee);
+   // scanf("%d %s %d", &pDate->jour, &pDate->mois, &pDate->annee);
+
+   printf("\n");
+}
+
+
+
+//Question B]Affiche la date passée en paramètre
+
+void afficheDate(Date *pDate) {
+   printf("\nLa date est: %d /%d /%d", pDate->jour, pDate->mois, pDate->annee);
+}
+
+//Question C]Creation d'une date et son initialisation
+
+Date creerDateParCopie () {
+   Date nouvelleDate;
+   Date* pNouvelleDate = &nouvelleDate;
+   initialiseDate(pNouvelleDate);
+   return nouvelleDate;
+}
+
+// Question D] 
+Date* newDate() {
+   
+}
