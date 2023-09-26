a80798acfa2ec9a6f532963b3a2dc408f5ae4e0c
Patarot <adele.patarot@gmail.com>
2023-09-26 17:33:37 +0200

    TP3 turing version1

--- TP-03-PrixTuring/prixTuring.c
+++ TP-03-PrixTuring/prixTuring.c
@@ -3,57 +3,203 @@
  gcc --std=c99 -W -Wall -o prixTuring prixTuring.c
 
  Exécution
- ./prixTuring < turingWinners.txt > out.txt
+ ./prixTuring
 
  Tests
- diff out.txt turingWinners.txt
+ diff out.csv turingWinners.csv
 
- Détection de fuites mémoires
- valgrind --leak-check=yes --leak-check=full --show-leak-kinds=all --show-reachable=no ./prixTuring < turingWinners.txt > out.txt
 **/
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
+#include <assert.h>
 #include <stdbool.h>
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// VARIABLES
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-/* This function scans a line of text (until \n) and returns a char* that contains all characters on the line (up to 255) excluding \n.
-It also ensures the \0 termination.
-**WARNING**: The result of this function has been allocated (calloc) by the function */
-char* scanLine()
-{
-	int maxLineSize = 255;
-	char c, *line = calloc(maxLineSize+1,sizeof(char));
+//Definition de la struct Data: les différentes informations du fichier
+typedef struct{
+	int annee;
+	char *nomWinner;
+	char *natures;
+}  Winner; 
 
-	scanf("%250[^\n]", line);
+//Structures comprenants toutes les data stockées en memoire
+Winner* dataWinners; 
 
-	if ( (c = getchar()) != '\n') {
-		/* we did not get all the line */
-		line[250] = '[';
-		line[251] = line[252] = line[253] = '.';
-		line[254] = ']';
-		// line[255] = '\0'; // useless because already initialized by calloc
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//FONCTIONS
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-		// let's skip all chars untli the end of line
-		while (( (c = getchar()) != '\n') && c != EOF) ;
+//valideFichier
+// Renvoie true si bonne affectation, false sinon
+bool fichierValide(FILE * fichier, char fileName[]) {
+	bool resultat=false;
+	if(fichier != NULL) {
+		resultat=true;
+	} else {
+		printf("Erreur avec le fichier %s",fileName);
+	}
+	return resultat;
+}
+
+// numberOfWinners 
+//prend en paramètre le fichier de données (FILE) et retourne le nombre de ligne qu’il contient
+int numberOfWinners(FILE* fichier) {
+	int resultat=0;
+	char caractere;
+	
+	//lire pour connaitre le nombre de ligne
+	while((caractere=fgetc(fichier)) != EOF) {
+		if(caractere == ';'){
+			resultat++;
+		}
 	}
 
-	return line;
+	return resultat/2;
 }
 
-/* This function scans a line of text (until \n), converts it as an integer and returns this integer */
-int scanLineAsInt() {
-	int buf;
-	scanf("%i\n",&buf);
-	return buf;
+//afficheNumberOfWinners
+void afficheNumberOfWinners(int number) {
+	printf("Nombre de ligne du fichier= %d\n", number);
 }
 
 
-int main(void)
+// numberOfChar
+int numberOfChar(char chaine[]) {
+	int i=0;
+	int resultat = 0;
+	while(chaine[i] != ';') {
+		i++;
+		resultat++;
+	}
+	return resultat;
+
+}
+
+void attribuerDate(char dateChaine[], int numeroWinner, Winner* dataWinners) {
+	int date= atoi(dateChaine);
+	dataWinners[numeroWinner].annee=date;
+	printf("winner numero %d date = %d\n", numeroWinner, dataWinners[numeroWinner].annee);
+}
+
+void attribuerNom(char nomChaine[], int numeroWinner, Winner* dataWinners) {
+	dataWinners[numeroWinner].nomWinner=nomChaine;
+	printf("winner numero %d nom = %s\n", numeroWinner, dataWinners[numeroWinner].nomWinner);
+}
+
+void attribuerNature(char nomChaine[], int numeroWinner, Winner* dataWinners) {
+	dataWinners[numeroWinner].natures=nomChaine;
+	printf("winner numero %d nature = %s\n", numeroWinner, dataWinners[numeroWinner].natures);
+}
+
+
+			
+//readWinners 
+//lit et stocke en memoire les info du fichier, dans le tableau datas
+void readWinners(FILE* fichier) {
+	//lecture du fichier
+	char caractere;
+	while((caractere=fgetc(fichier)) != EOF) {
+		printf("%c", caractere);
+	}
+	rewind(fichier);
+
+	//stockage des infos dans le tableau de data
+
+	//numero du winner en cours de lecture
+	int numeroWinner=0;
+	//0: date; 1: nomWinner; 2:nature
+	int numeroColonne=0;
+	//caractere en cours de lecture
+	char caractere2;
+	
+		for(numeroColonne=0; numeroColonne<=2;numeroColonne++){
+			//Declaration et Initilisation du buffer
+			char buffer[500];
+			memset(buffer,0,sizeof(buffer));
+
+			//Nombre de caractere de notre type de données
+			int nombreCaractere=0;
+
+			//On parcours le fichier jusqu'à rencontrer un ;
+			while((caractere2=fgetc(fichier)) != EOF) {
+				if(caractere2 != ';'){
+					buffer[nombreCaractere]=caractere2; //On remplie le buffer des caractères en cours de lectures
+					nombreCaractere++;
+				} else {
+					break; //Sortie de boucle si rencontre d'un ;
+				}
+			}
+		
+			//On marque la fin de la chaine de caractère
+			buffer[nombreCaractere+1]='\0';
+
+			//attribution en fonction du type de données
+			if(numeroColonne==0) {
+				attribuerDate(buffer, numeroWinner,dataWinners);
+			} else if( numeroColonne == 1) {
+				attribuerNom(buffer, numeroWinner,dataWinners);
+			} else {
+				attribuerNature(buffer, numeroWinner,dataWinners);
+			}
+		}
+}
+
+// printWinners
+//prend les donnes data et vient les ecrires dans fichier
+
+void printWinners(FILE* Fichier, Winner* datas) {
+	// TO DO
+}
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// MAIN
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+int main(int argc, char** argv)
 {
+	char filename[] = "turingWinners.csv";
+	char outputFilename[] = "out.csv";
 
-	int nbGagnants = scanLineAsInt();
-	printf("nbGagnants = %i\n",nbGagnants);
+   //Declaration des variables fichiers
+	FILE * turingWinners = fopen(filename, "r");
+	FILE * out = fopen(outputFilename, "r");
+
+	//test fonction number of winner
+	// //Si l'ouverture du fichier a bien fonctionne
+	// if(fichierValide(turingWinners, filename)) {
+	// 	// readWinners(turingWinners);
+	// 	printf("Fichier original\n");
+	// 	afficheNumberOfWinners(numberOfWinners(turingWinners));
+	// } 
+
+	// if(fichierValide(out, outputFilename)) {
+	// 	printf("Fichier out\n");
+	// 	afficheNumberOfWinners(numberOfWinners(out));
+	// }
+
+
+	if(fichierValide(turingWinners,filename) && fichierValide(out, outputFilename)) {
+		//Allocation dynamique en mémoire de datas
+		int nb = numberOfWinners(turingWinners);
+		dataWinners = (Winner *)malloc(nb*sizeof(Winner));
+
+		//Lis le fichier + stocke en memoire les infos dans dataWinners
+		readWinners(turingWinners);	
+	}
+	
+
+	
+	//Fermeture des fichiers
+	fclose(turingWinners);
+	fclose(out);
+
+	//liberation de la memoire
+	free(dataWinners);
 
 	return EXIT_SUCCESS;
 }
