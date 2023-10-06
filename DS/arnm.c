169f6704a73404b820fe0a1c8caffde930ebb75e
Patarot <adele.patarot@gmail.com>
2023-10-06 11:24:00 +0200

    Exercice ARN terminee

--- DS/arnm.c
+++ DS/arnm.c
@@ -1,7 +1,10 @@
+//Adele PATAROT
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <math.h>
 #include "vaccins_covid.h"
 
 // =========
@@ -9,14 +12,17 @@
 // =========
 
 const char NUCLEOTIDES[] = "ATCG";
+typedef struct {
+	int index;
+	int distance;
+} Difference ;
 
 // =============
 // = Fonctions =
 // =============
 
-
 bool isNucleotide(char c) {
-    return (c==NUCLEOTIDES[0] || c==NUCLEOTIDES[1] || c==NUCLEOTIDES[2] || c==NUCLEOTIDES[3]);
+   return (c==NUCLEOTIDES[0] || c==NUCLEOTIDES[1] || c==NUCLEOTIDES[2] || c==NUCLEOTIDES[3]);
 }
 
 int indexOf(char nucleotide) {
@@ -31,9 +37,79 @@ int indexOf(char nucleotide) {
 }
 
 int distanceBetweenNucleotides(char n1, char n2) {
-    return indexOf(n1)-indexOf(n2);
+   return indexOf(n1)-indexOf(n2);
 }
 
+double* statistics(char chaine[]){
+	//initialisation du tableau de resultat
+	double resultat[4]={0,0,0,0};
+	double* pointeur=resultat;
+	
+	for(int i=0; i<strlen(chaine); i++){
+		if(isNucleotide(chaine[i])){
+			if(chaine[i]=='A'){
+				resultat[0]++;
+			} else if (chaine[i]=='T'){
+				resultat[1]++;
+			} else if (chaine[i]=='C'){
+				resultat[2]++;
+			} else if (chaine[i]=='G'){
+				resultat[3]++;
+			}
+		}
+	}
+
+	//En effectue le calcul du pourcentage
+	for(int i=0; i<4; i++){
+		resultat[i]=(resultat[i]/strlen(chaine))*100;
+	}
+
+	return pointeur;
+}
+
+Difference* newDifference(){
+	Difference* new = (Difference*)malloc(sizeof(Difference));
+	return new;
+
+}
+
+Difference* initiDifference(int index, int distance, Difference* new){
+	new->distance=distance;
+	new->index=index;
+	return new;
+}
+
+void computeDifferenceBetween(const char seq1[], const char seq2[], Difference* diff[]){
+	//Taille des deux chaines de caractères
+	int lenght=strlen(seq1);
+	// printf("taille=%d", lenght);
+	int nbDiff=0;
+	//Recherche de différence
+	for(int i=0; i<=lenght; i++){
+		//Couple de recherche pour une difference, reinitialisé à chaque itération
+		int index=0;
+		int distance=0;
+		if(isNucleotide(seq1[i]) && isNucleotide(seq2[i])){ // Verifi caractères = nucléotides
+			if (seq1[i]!=seq2[i]) { // Si différence
+				// printf("jai trouve 1 diff\n");
+				index=i;
+				distance=distanceBetweenNucleotides(seq1[i], seq2[i]);
+				//Ajout de cette nouvelle différence dans notre tableau de difference
+				diff[nbDiff]=initiDifference(index,distance,newDifference());
+				// printf("Diff num %d: distance=%d et index =%d\n", nbDiff,diff[nbDiff]->distance, diff[nbDiff]->index);
+				nbDiff++;
+			}
+		}
+	}
+}
+
+void printDifferences(Difference** differences){
+	int i=0;
+	while(differences[i]!=NULL){
+		printf("Difference numero %d: <%d,%d>\n",i+1,differences[i]->index,differences[i]->distance);
+		i++;
+	}
+}
 
 // ========
 // = Main =
@@ -42,8 +118,23 @@ int distanceBetweenNucleotides(char n1, char n2) {
 int main(void){
 
 	char sequenceDeNucleotides[]="ATGCATCCATCATGCATGTA";
+	// const char seq1[]="ATGC";
+	// const char seq2[]="ATCG";
+	
+	printf("Dans la sequence: %s\n",sequenceDeNucleotides);
+	printf("%f pourcent de A\n", statistics(sequenceDeNucleotides)[0]); //A
+	printf("%f pourcent de T\n", statistics(sequenceDeNucleotides)[1]); //T
+	printf("%f pourcent de C\n", statistics(sequenceDeNucleotides)[2]); //C
+	printf("%f pourcent de G\n", statistics(sequenceDeNucleotides)[3]); //G
 
-    printf("%s\n",code_proteine_spike_moderna);
+	printf("\n\n");
+	//Initialiser dynamiquement avec un malloc le tableau de différences
+	int taillemax=strlen(code_proteine_spike_moderna); // Nombre maxi de differences
+	Difference** differences= (Difference**)malloc(taillemax*sizeof(Difference*));
+	computeDifferenceBetween(code_proteine_spike_pfizer, code_proteine_spike_moderna, differences);
+	printf("Pour les sequences code_proteine_spike_pfizer &  code_proteine_spike_moderna\n");
+	printDifferences(differences);	
+	free(differences);
 
 	return EXIT_SUCCESS;
 }
